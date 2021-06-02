48bd7f1d51ba38f292450e396f3761be6d9c7300
Wille Karvinen <wille.karvinen@gmail.com>
2021-06-02 21:39:20 +0300

    Added jimms.fi auto-buy

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
     let urls = ["https://www.jimms.fi/fi/Product/List/000-1N0/komponentit--naytonohjaimet--geforce-rtx-pelaamiseen?ob=8",
-        "https://www.verkkokauppa.com/fi/catalog/706b/Naytonohjaimet/products?query=RTX+3070+Ti&sort=releaseDate%3Adesc",
-        "https://www.gigantti.fi/INTERSHOP/web/WFS/store-gigantti-Site/fi_FI/-/EUR/ViewStandardCatalog-Browse?CategoryName=fi-tietokonekomponentit-naytonohjaimet&CategoryDomainName=store-gigantti-ProductCatalog&SearchParameter=%26%40QueryTerm%3D*%26ContextCategoryUUID%3DYlOsGQV5I1EAAAFa8VOVoDxg%26discontinued%3D0%26online%3D1&SortingAttribute=ACTdate-desc&select-sort-refine=ACTdate-desc",
-        "https://www.proshop.fi/Naeytoenohjaimet?o=2304",
-        "https://www.power.fi/c/3677/tietotekniikka/tietokonekomponentit/naytonohjaimet/",
+       // "https://www.verkkokauppa.com/fi/catalog/706b/Naytonohjaimet/products?query=RTX+3070+Ti&sort=releaseDate%3Adesc",
+      //  "https://www.gigantti.fi/INTERSHOP/web/WFS/store-gigantti-Site/fi_FI/-/EUR/ViewStandardCatalog-Browse?CategoryName=fi-tietokonekomponentit-naytonohjaimet&CategoryDomainName=store-gigantti-ProductCatalog&SearchParameter=%26%40QueryTerm%3D*%26ContextCategoryUUID%3DYlOsGQV5I1EAAAFa8VOVoDxg%26discontinued%3D0%26online%3D1&SortingAttribute=ACTdate-desc&select-sort-refine=ACTdate-desc",
+      //  "https://www.proshop.fi/Naeytoenohjaimet?o=2304",
+      //  "https://www.power.fi/c/3677/tietotekniikka/tietokonekomponentit/naytonohjaimet/",
         "https://www.nvidia.com/fi-fi/geforce/graphics-cards/30-series/rtx-3070-3070ti/#wtb"
     ]
     for (i = 0; i < urls.length; i++) {
--- content.js
+++ content.js
@@ -1,23 +1,87 @@
 let currentWebsite = window.location.toString()
 
 let RTXIcon = document.createElement("img")
-    RTXIcon.src = chrome.runtime.getURL("media/icon.png")
-    RTXIcon.style.height = "35px"
-    RTXIcon.style.width = "35px"
-    RTXIcon.style.float = "left"
-
+RTXIcon.src = chrome.runtime.getURL("media/icon.png")
+RTXIcon.style.height = "35px"
+RTXIcon.style.width = "35px"
+RTXIcon.style.marginRight = "1px"
+RTXIcon.style.float = "left"
 
 if (currentWebsite.includes("jimms")) {
-    let addIcon = document.querySelector(".navbar-nav")
-    addIcon.prepend(RTXIcon)
+    if (currentWebsite.includes("jimms.fi/fi/CheckOut") || currentWebsite.includes("jimms.fi/fi/ShoppingCart")) {
+        let basketNext = setInterval(() => {
+            if (document.querySelector("#cart").className == "tab-pane active") {
+                document.querySelectorAll(".btn.btn-success")[2].click()
+                clearInterval(basketNext)
+            }
+        }, 1);
 
-    document.querySelector(".btn.btn-success.btn-sm").addEventListener("click", function() {
-        console.log("BRUH")
-        chrome.tabs.create({url: "https://www.jimms.fi/fi/CheckOut/Customer"})
-    })
+        let infoNext = setInterval(() => {
+            if (document.querySelector("#customer").className == "tab-pane active") {
+                document.querySelectorAll(".btn.btn-success")[4].click()
+                clearInterval(infoNext)
+            }
+        }, 1);
 
-    console.log("JIMMS")
-}
-else if (currentWebsite.includes("gigantti")){
-    console.log("GIGANTTI")
+        let deliveryNext = setInterval(() => {
+            if (document.querySelector("#delivery").className == "tab-pane active" && document.querySelector("#delivery h4") && !document.querySelector("#delivery > div.alert.alert-info")) {
+                document.querySelector("#DeliveryMethodID").remove()
+                document.querySelector("#DeliveryMethodID").remove()
+                document.querySelector("#DeliveryMethodID").remove()
+                document.querySelector("#DeliveryMethodID").checked = "true"
+                document.querySelectorAll(".btn.btn-success")[6].click()
+                clearInterval(deliveryNext)
+            }
+        }, 1);
+
+        let postNext = setInterval(() => {
+            if (document.querySelector("#delivery").className == "tab-pane active" && document.querySelector("#delivery h3")) {
+                document.querySelector("#SPID").checked = "true"
+                document.querySelectorAll(".btn.btn-success")[7].click()
+                clearInterval(postNext)
+            }
+        }, 1);
+
+
+        let paymentNext = setInterval(() => {
+            if (document.querySelector("#delivery").className == "tab-pane active" && document.querySelector("#delivery h4") && document.querySelector("#delivery > div.alert.alert-info")) {
+                document.querySelector("#PaymentMethodID").checked = "true"
+                document.querySelectorAll(".btn.btn-success")[6].click()
+                clearInterval(paymentNext)
+            }
+        }, 1);
+
+        let mobilepayNext = setInterval(() => {
+            if (document.querySelector("#verify").className == "tab-pane active") {
+                document.querySelectorAll(".btn-payment.payment-image")[11].click()
+                clearInterval(mobilepayNext)
+            }
+        }, 1);
+    } 
+    
+    else {
+        let basketButtons = document.getElementsByClassName("btn btn-success btn-sm")
+        for (i = 0; i < basketButtons.length; i++) {
+            basketButtons[i].addEventListener("click", function () {
+                window.open("https://www.jimms.fi/fi/CheckOut/Customer")
+
+            })
+        }
+        let addIcon = document.querySelector(".navbar-nav")
+        addIcon.prepend(RTXIcon)
+    }
+} 
+
+else if (currentWebsite.includes("mobilepay")) {
+    let mobilepayPayment = setInterval(() => {
+        if (document.querySelector("body > app-shell")) {
+            if (document.querySelector("body > app-shell").shadowRoot.querySelector("request-page")) {
+                if (document.querySelector("body > app-shell").shadowRoot.querySelector("request-page").shadowRoot.querySelector("standard-page > section:nth-child(2) > box-content").shadowRoot.querySelector("core-button").shadowRoot.querySelector("a")) {
+                    let button = document.querySelector("body > app-shell").shadowRoot.querySelector("request-page").shadowRoot.querySelector("standard-page > section:nth-child(2) > box-content").shadowRoot.querySelector("core-button").shadowRoot.querySelector("a")
+                    button.click()
+                    clearInterval(mobilepayPayment)
+                }
+            }
+        }
+    }, 1);
 }
